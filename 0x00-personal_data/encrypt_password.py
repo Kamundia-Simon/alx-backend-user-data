3ad1de38c475a0e5c267f86716f996c03487d3cd
Kamundia-Simon <simonkamundia8@gmail.com>
2024-11-07 23:41:45 +0300

    check hashed pass

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,9 +4,15 @@ encrypt password
 """
 import bcrypt
 
-
 def hash_password(password: str) -> bytes:
     """
     Hash a password with a randomly generated salt
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    validate hashed password
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
