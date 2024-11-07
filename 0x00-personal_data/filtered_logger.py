d5beb340dc3db90aa5c0dca1ab56002c56245a47
Kamundia-Simon <simonkamundia8@gmail.com>
2024-11-07 21:54:17 +0300

    filter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,7 +2,7 @@
 """ filtering sensitive fields in log messages"""
 import re
 from typing import List
-
+import logging
 
 patterns = {
     'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
@@ -19,3 +19,22 @@ def filter_datum(
     """Obfuscates specified fields."""
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """class that redacts specified fields in log messages"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialize RedactingFormatter"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format the log record by redacting fields"""
+        record.msg = filter_datum(
+                self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super().format(record)
