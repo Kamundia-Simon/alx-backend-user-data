ec968698c9d6cf3b287855bd70dafa099f230b85
Kamundia-Simon <simonkamundia8@gmail.com>
2024-11-21 16:19:19 +0300

    Hash a password with bcrypt

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Hash Password
+"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """hash a password using bcrypt"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
