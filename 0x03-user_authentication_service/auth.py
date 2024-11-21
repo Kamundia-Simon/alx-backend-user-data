622ce6856893b9c9cdeddefe27b0f9a4e05207ed
Kamundia-Simon <simonkamundia8@gmail.com>
2024-11-21 18:34:39 +0300

    generate uuid

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -15,18 +15,19 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
+def _generate_uuid() -> str:
+    """
+    Generate a new UUID and return str represntation
+    """
+    return str(uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
 
     def __init__(self):
         self._db = DB()
-
-    def _generate_uuid() -> str:
-    """
-    Generate a new UUID and return str represntation
-    """
-    return str(uuid4())
 
     def register_user(self, email: str, password: str) -> User:
         """register new user with given password and email"""
